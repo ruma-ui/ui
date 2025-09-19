f5e5921e6b78c8cab42d26469e361d1849514baa
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2025-09-19 22:53:17 +0530

    feat: add passWithNoTests option to jest configuration files

--- ui/jest.config.ts
+++ ui/jest.config.ts
@@ -1,6 +1,7 @@
 export default {
   displayName: "ui",
   preset: "../jest.preset.js",
+  passWithNoTests: true,
   transform: {
     "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nx/react/plugins/jest",
     "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nx/react/babel"] }],
--- utils/jest.config.ts
+++ utils/jest.config.ts
@@ -2,6 +2,7 @@ export default {
   displayName: "utils",
   preset: "../jest.preset.js",
   testEnvironment: "node",
+  passWithNoTests: true,
   transform: {
     "^.+\\.[tj]s$": ["ts-jest", { tsconfig: "<rootDir>/tsconfig.spec.json" }],
   },
