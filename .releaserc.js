fccc85ccd9f26bcea5dc7689b83027bae0ae2a90
Ranit Manik <138437760+RanitManik@users.noreply.github.com>
2026-07-30 01:36:30 +0530

    chore: configure semantic-release to include package.json

--- .releaserc.js
+++ .releaserc.js
@@ -62,5 +62,12 @@ module.exports = {
       },
     ],
     "@semantic-release/github",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md", "package.json"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
   ],
 };
